8379504b8c3ee3fbea812cdecf80f0f126134d9f
servala <marina.severyanova@gmail.ru>
2012-10-11 03:00:08 +0400

    список лекций. формат данных

---
+++ data.js
@@ -0,0 +1,239 @@
+var calendar = { 
+        'years': { 
+            '2012': { 
+                'months': { 
+                    '8': { 
+                        'days': {
+                            '15': {
+                                'decoration': 'red',
+                                'lectures': [{
+                                    'time': '12:00',
+                                    'caption': 'Общий цикл разработки',
+                                    'author': {
+                                        'name': 'Михаил Трощев',
+                                        'link': 'http://clubs.ya.ru/4611686018427468886/replies.xml?item_no=77'
+                                    },
+                                    'description': 'Всем привет. Школу мы проводим впервые, поэтому хотим, чтобы не	только мы помогали вам развиваться, но и наоборот.' +
+                                             ' Мы планируем по	каждой лекции собирать ваши замечания, понять понравился ли вам	рассказ, как вы оцениваете лекторов.' +
+                                             ' В опросе будут чекбоксы, но	хотелось бы увидеть и развернутые ответы в комментариях.' +
+                                             '<br/>' +
+                                             'Итак, первая лекция — «Общий цикл разработки»',
+                                    'prezentation': 'http://yadi.sk/d/VDsJ4ZUBiq6u',
+                                    'video': [ null, null ],
+                                    'history': 'red'
+                                }, {
+                                    'time': '13:00',
+                                    'caption': 'Системы ведения задач',
+                                    'author': {
+                                        'name': 'Сергей Бережной',
+                                        'link': 'http://clubs.ya.ru/4611686018427468886/replies.xml?item_no=89'
+                                    },
+                                    'description': '',
+                                    'prezentation': 'http://yadi.sk/d/D5xTwoIciq6c',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }, {
+                                    'time': '14:00',
+                                    'caption': 'Wiki',
+                                    'author': {
+                                        'name': 'Сергей Бережной',
+                                        'link': 'http://clubs.ya.ru/4611686018427468886/replies.xml?item_no=89'
+                                    },
+                                    'description': 'Друзья, очень важно ваше мнение. Veged, «Wiki».',
+                                    'prezentation': 'http://yadi.sk/d/D5xTwoIciq6c',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }]
+                            },
+                            '18': {  
+                                'decoration': 'red',
+                                'lectures': [{
+                                    'time': '19:00',
+                                    'caption': 'Командная строка Unix',
+                                    'author': {
+                                        'name': 'Виктор Ашик',
+                                        'link': null
+                                    },
+                                    'description': 'Оставляем отзывы',
+                                    'prezentation': 'http://yadi.sk/d/3N0d6h9rlRA8',
+                                    'video': [ 'http://static.video.yandex.ru/lite/ya-events/qxv95xwi4q.4820', 'http://yadi.sk/d/O5OBYxHwnGTw' ],
+                                    'history': null
+                                }, {
+                                    'time': '20:00',
+                                    'caption': 'Редакторы/Средства разработки',
+                                    'author': {
+                                        'name': 'Вячеслав Олиянчук',
+                                        'link': 'http://miripiruni.ya.ru/'
+                                    },
+                                    'description': 'Оставляем отзывы. Что было приятного в докладе, что бы еще хотелось услышать.',
+                                    'prezentation': 'https://github.com/yandex-shri/lectures/blob/master/05-editors.md',
+                                    'video': [ 'http://static.video.yandex.ru/lite/ya-events/h4kt5t9a07.4101', 'http://yadi.sk/d/f3CYmlG3nGSC' ],
+                                    'history': null
+                                }]
+                            }
+                        }
+                    },
+                    '9': {
+                        'days': {
+                            '2': {  
+                                'decoration': 'red',
+                                'lectures': [{  
+                                    'time': '19:00',
+                                    'caption': 'Отладка кода',
+                                    'author': {
+                                        'name': 'Алексей Андросов',
+                                        'link': null
+                                    },
+                                    'description': 'Привет, оцениваем лекцию!',
+                                    'prezentation': 'http://yadi.sk/d/5fTEBWfu01pFQ',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }, {
+                                    'time': '20:00',
+                                    'caption': 'СSS',
+                                    'author': {
+                                        'name': 'Михаил Трошев',
+                                        'link': 'http://clubs.ya.ru/4611686018427468886/replies.xml?item_no=77'
+                                    },
+                                    'description': '',
+                                    'prezentation': '',
+                                    'video': [ null, null ],
+                                    'history': 'cansel'
+                                }]
+                            },
+                            '4': {
+                                'decoration': 'red',
+                                'lectures': [{
+                                    'time': '19:00',
+                                    'caption': 'Клиентская оптимизация',
+                                    'author': {
+                                        'name': 'Ивана Карева',
+                                        'link': null
+                                    },
+                                    'description': 'Не забываем оставлять отзывы!',
+                                    'prezentation': 'http://yadi.sk/d/S3dVJJdL01pTk',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }, { 
+                                    'time': '20:00',
+                                    'caption': 'Профилирование',
+                                    'author': {
+                                        'name': 'Михаил Корепанов',
+                                        'link': null
+                                    },
+                                    'description': null,
+                                    'prezentation': 'http://yadi.sk/d/1EjnM84h01plG',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }]
+                            },
+                            '6': {   
+                                'decoration': 'red',
+                                'lectures': [{
+                                    'time': '12:00',
+                                    'caption': 'Регулярные выражения',
+                                    'author': {
+                                        'name': 'Максим Ширшин',
+                                        'link': null
+                                    },
+                                    'description': 'Лекцию читал Максим Ширшин. Оставляйте ваши пожелания/ отзывы',
+                                    'prezentation': 'http://yadi.sk/d/cPE_GA-w06Iag',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }, {
+                                    'time': '13:00',
+                                    'caption': 'CSS',
+                                    'author': {
+                                        'name': 'Михаил Трощев',
+                                        'link': 'http://clubs.ya.ru/4611686018427468886/replies.xml?item_no=77'
+                                    },
+                                    'description': null,
+                                    'prezentation': 'http://yadi.sk/d/1XZK0I1a05xaK',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }]
+                            },
+                            '9': {
+                                'decoration': 'red',
+                                'lectures': [{
+                                    'time': '19:00',
+                                    'caption': 'Фреймворки',
+                                    'author': {
+                                        'name': 'Алексей Андросова',
+                                        'link': null
+                                    },
+                                    'description': 'Как вам обзор от Леши Андросова?',
+                                    'prezentation': 'http://yadi.sk/d/92XeomWV06mg2',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }, {
+                                    'time': '20:00',
+                                    'caption': 'jQuery',
+                                    'author': {
+                                        'name': 'Алексей Бережной.',
+                                        'link': null
+                                    },
+                                    'description': null,
+                                    'prezentation': 'http://yadi.sk/d/4zPgYu1V06nAc',
+                                    'video': [ null, null ],
+                                    'history': null
+                                }]
+                            },
+                            '11': {
+                                'decoration': 'red',
+                                'lectures': [{
+                                    'time': '19:00',
+                                    'caption': 'БЭМ (2 лекции)',
+                                    'author': {
+                                        'name': 'Владимир Варанкин',
+                                        'link': null
+                                    },
+                                    'description': null,
+                                    'prezentation': null,
+                                    'video': [ null, null ],
+                                    'history': null
+                                }]
+                            },
+                            '13': {
+                                'decoration': 'red',
+                                'lectures': [{
+                                    'time': '12:00',
+                                    'caption': 'Шаблонизаторы',
+                                    'author': {
+                                        'name': 'Сергей Бережной',
+                                        'link': 'http://clubs.ya.ru/4611686018427468886/replies.xml?item_no=89'
+                                    },
+                                    'description': null,
+                                    'prezentation': null,
+                                    'video': [ null, null ],
+                                    'history': null
+                                }, {
+                                    'time': '13:00',
+                                    'caption': 'Дизайн',
+                                    'author': {
+                                        'name': 'Константин Горский',
+                                        'link': null
+                                    },
+                                    'description': null,
+                                    'prezentation': null,
+                                    'video': [ null, null ],
+                                    'history': null
+                                }, {
+                                    'time': '14:00',
+                                    'caption': 'Дизайн глазами разработчика',
+                                    'author': {
+                                        'name': 'Михаил Трошев',
+                                        'link': 'http://clubs.ya.ru/4611686018427468886/replies.xml?item_no=77'
+                                    },
+                                    'description': null,
+                                    'prezentation': null,
+                                    'video': [ null, null ],
+                                    'history': null
+                                }]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
